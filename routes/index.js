e34914be21e8af96ab61acd4a0595f01a245c3f9
harsh vardhan <harshvardhanbahree@gmail.com>
2024-06-08 23:59:13 +0530

    Removing unncessary files

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controller/url");
-
-router.post("/", controller.generateShortUrl);
-
-module.exports = {
-  router,
-};
